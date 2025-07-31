93b4d05bf1bbc19f84c9b0c31be929397a4a8e49
vitoralexandreeggers <eggers.vitor@escola.pr.gov.br>
2025-07-31 11:51:52 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# primeiro_css
+# juros
--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-alert("TESTE CAPSLOCK");
 
-let nome = prompt("digite seu nome: ");
 
-alert("seu nome é " + nome);
+function calcula(){
+
+    let c = parseFloat(document.getElementById('numero_C').value);
+    let i = parseFloat(document.getElementById('numero_I').value);
+    let t = parseFloat(document.getElementById('numero_T').value);
+   
+
+    i = i / 100;
+
+    let J = c * i * t;
+    
+    
+       
+    document.getElementById('juros').textContent = 'juros: ' + J;
+
+    let m = c + J
+    document.getElementById('montante').textContent = 'montante: ' + m;
+
+    
+
+   }
+
+
+
+
+
+
+   
